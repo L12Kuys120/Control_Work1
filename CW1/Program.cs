c195940f92d1fcf2dd977f1c078860544d81c1d9
Mikhail <radionetcom@inbox.ru>
2023-07-03 11:43:10 +0300

    Add folder CW1

---
+++ CW1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Control work
+Console.WriteLine("Enter size of array more 2");
+int N = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter array");
+string[] array = new string[N];  // Задание массива вручную
+for (int i = 0; i < N; i++) array[i] = Convert.ToString(Console.ReadLine());
+
+Console.WriteLine("");
+
+Console.WriteLine("[" + string.Join(" ", array) + "]");  //вывод массива в строку 
+Console.WriteLine("");
+
+int P = new Random().Next(0, 3); 
+string[] arr = new string[P];
+
+for (int i = 0; i < P; i++) arr[i] = array[i]; //Выборка Р-элементов
+
+Console.WriteLine("[" + string.Join(" ", arr) + "]"); 
+Console.WriteLine("");
